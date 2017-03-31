94692472cbbcc6881ea1ed20bc3554b6ffcfc4bd
581488 <581488@PC347121.cts.com>
2017-03-31 16:56:41 +0530

    Changes to TopNav.java

--- cwr-framework/core/src/main/java/com/aem/cwr/framework/core/TopNav.java
+++ cwr-framework/core/src/main/java/com/aem/cwr/framework/core/TopNav.java
@@ -7,23 +7,22 @@ public class TopNav extends WCMUsePojo {
 	String pagePath = "";
 	Page pageResource;
 	int pageDepth;
-	String currentPageResource;
+	Page currentPageResource;
 
 	public int getPageDepth() {
+		pageDepth = currentPageResource.getDepth();
 		return pageDepth;
 	}
-
 
 	public Page getPageResource() {
 		pageResource = this.getPageManager().getPage(pagePath);
 		return pageResource;
 	}
-	
-	
+
 	@Override
 	public void activate() {
 		pagePath = get("path", String.class);
-		currentPageResource= get("currentPageParam", String.class);
+		currentPageResource = this.getPageManager().getPage(get("currentPageParam", String.class));
 	}
 
 }
